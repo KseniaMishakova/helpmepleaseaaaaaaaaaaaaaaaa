34292f4016c38870c64449a0e8b0abf2c8d0f0a0
Ксения <goron3333@gmail.com>
2021-03-18 19:26:57 +0300

    1

--- test_flask.py
+++ test_flask.py
@@ -1,5 +1,3 @@
-import os
-
 from flask import Flask
 from flask_ngrok import run_with_ngrok
 
@@ -13,5 +11,4 @@ def index():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
